9876abd6c661caed8a88ed136acc4b1cf391d164
Boris's PC <mircealike2013@gmail.com>
2023-09-16 20:26:39 +0300

    Updated NotFound

--- client/src/Components/NotFound.jsx
+++ client/src/Components/NotFound.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from "react-router-dom";
 const NotFound = () => {
     const navigate = useNavigate();
     return (
-        <div className=" relative left-11 flex h-screen w-full items-center justify-center p-4">
+        <div className=" relative left-5 flex h-screen w-full items-center justify-center p-4">
             <div className="h-15vh w-50vw border-crunchyroll-orange bg-dark-gray border-4">
-                <text className="leading-2.1 drop-shadow-glow left-85 absolute z-10 text-5xl font-bold">
+                <text className="leading-2.1 drop-shadow-glow left-88 absolute z-10 text-5xl font-bold">
                     PAGE NOT FOUND
                 </text>
-                <text className="leading-2.1 text-crunchyroll-orange drop-shadow-glow left-86 absolute text-5xl font-bold">
+                <text className="leading-2.1 text-crunchyroll-orange drop-shadow-glow left-89 absolute text-5xl font-bold">
                     PAGE NOT FOUND
                 </text>
             </div>
@@ -20,17 +20,17 @@ const NotFound = () => {
             </text>
             <img
                 src="./Images/anime-cauldron-logo.webp"
-                className="left-85 absolute top-16 max-w-sm"
+                className="left-88 absolute top-16 max-w-sm"
             />
             <img
                 src="./Images/luffy_sad.webp"
-                className="-scale-y-flip absolute left-16 top-32 h-80"
+                className="-scale-y-flip absolute left-16 top-35 h-80"
             />
             <img
                 src="./Images/chopper_sad.webp"
-                className="bottom-46 absolute right-0 font-bold"
+                className="bottom-50 h-101 absolute right-7 font-bold"
             />
-            <p className="left-90 absolute bottom-72">
+            <p className="left-97 absolute bottom-72">
                 {" "}
                 <button
                     className="hover:text-blue-600"
