2e5959863b8b8e3510201215377456979e4991c6
Andra0011 <capraruandra@gmail.com>
2023-09-27 12:47:01 +0300

    ceva

--- client/src/Components/AnimeList.jsx
+++ client/src/Components/AnimeList.jsx
@@ -8,7 +8,13 @@ const AnimeList = () => {
     const nav = useNavigate();
     const [page, setPage] = useState(1); // Initialize the page state
 
-    const { data, isLoading, isError } = useQuery(['getAnime', page], () => axios.get(`https://api.jikan.moe/v4/anime?page=${page}`).then(resp => resp.data))
+    const { data, isLoading, isError } = useQuery(["getAnime", page], () =>
+        axios
+            .get(
+                `https://api.jikan.moe/v4/anime?page=${page}&order_by=title&sfw=true`
+            )
+            .then((resp) => resp.data)
+    );
 
     const changePage = (newPage) => {
         setPage(newPage);
@@ -23,11 +29,28 @@ const AnimeList = () => {
     }
 
     const animeList = data ? JSON.stringify(data) : "No data";
-    console.log(data)
+    console.log(data.data);
 
     return (
         <div className="relative h-full w-full">
             <Toolbar />
+            <div className="flex grid-cols-5 grid-rows-5 inline-grid">
+                {data.data.map((anime, i) => (
+                    <div
+                        key={i}
+                        className="text-white flex flex-col justify-center items-center w-64"
+                    >
+                        <div className="h- border-2 border-crunchyroll-orange">
+                            <img
+                                src={anime.images.webp.image_url}
+                                alt={anime.title}
+                                
+                            />
+                        </div>
+                        <p className="text-white"> {anime.title} </p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
